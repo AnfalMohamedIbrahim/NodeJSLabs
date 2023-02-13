b495f288d9912141b0f381bc5e257443409a6537
AnfalMohamedIbrahimAhmed <anfal.mohamed38@gmail.com>
2023-02-13 16:25:17 +0200

    Day 1 Lab 1

---
+++ lab1.js
@@ -0,0 +1,63 @@
+console.log("hi from cli : command line interface :D ");
+ const fs = require("fs"); // to get the file system library 
+ 
+ //fs.writeFileSync("studentsGrades.txt","[]");  // to create the file 
+
+//  to put the [[studentId(autoincrement),studentName,studentGrade]]
+
+// to add 
+// console.log(process.argv[2]);
+var incrementalId = 0;
+if(process.argv[2] == "add"){
+    let data = JSON.parse(fs.readFileSync("studentsGrades.txt","utf-8"));
+    incrementalId=data.length;
+    let studentData={id : incrementalId , name : process.argv[3] , grade : process.argv[4] };
+    data.push(studentData);
+    fs.writeFileSync("studentsGrades.txt",JSON.stringify(data));
+    
+}
+// to edit the grade of the student by id 
+else if(process.argv[2] == "edit"){
+
+    let data = JSON.parse(fs.readFileSync("studentsGrades.txt","utf-8"));
+    editedId=parseInt(process.argv[4]);
+    console.log(data[editedId].name);
+    if(editedId==data[editedId].id){
+        data[editedId].grade = process.argv[3];
+        console.log(data[editedId].grade);
+      
+        fs.writeFileSync("studentsGrades.txt",JSON.stringify(data));
+    }
+  
+    
+}
+// to list the students 
+else if (process.argv[2] == "list"){
+    let data = JSON.parse(fs.readFileSync("studentsGrades.txt","utf-8"));
+      data.forEach(student => {
+        console.log("student Id : " ,student.id );
+        console.log("student name : " ,student.name );
+        console.log("student grade : " ,student.grade );
+        console.log(student);
+      });
+    console.log(data);
+}
+// to delete a student 
+else if(process.argv[2] == "delete"){
+
+    let data = JSON.parse(fs.readFileSync("studentsGrades.txt","utf-8"));
+    deletedId=parseInt(process.argv[3]);
+    console.log(data[deletedId].name);
+    if( deletedId==data[deletedId].id){
+
+      data.splice(deletedId,1);
+        fs.writeFileSync("studentsGrades.txt",JSON.stringify(data));
+    }
+  
+    
+}
+
+
+
+
+
